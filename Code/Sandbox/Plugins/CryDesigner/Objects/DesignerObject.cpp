c40ffb2c1ade84c34ea8dc2c845cd9d8f850fb8c
Ilya <chernecoff@gmail.com>
2020-07-08 16:10:33 +0300

    Update DesignerObject.cpp

--- Code/Sandbox/Plugins/CryDesigner/Objects/DesignerObject.cpp
+++ Code/Sandbox/Plugins/CryDesigner/Objects/DesignerObject.cpp
@@ -687,8 +687,6 @@ void DesignerObjectFlags::Update()
 	ModifyFlag<uint64>(nFlags, ERF_FOB_ALLOW_DECAL_BLEND, !ignoreDecalBlend);
 	ModifyFlag<int>(statobjFlags, STATIC_OBJECT_NO_PLAYER_COLLIDE, excludeCollision);
 	
-	const bool isPhysicalizated = m_pObj->GetCompiler()->CheckFlags(ECompilerFlag::eCompiler_Physicalize);
-
 	if (disablePhysicsProxy)
 	{
 		m_pObj->GetCompiler()->RemoveFlags(ECompilerFlag::eCompiler_Physicalize);
