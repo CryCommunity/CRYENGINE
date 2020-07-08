78f26208e91330bef8d680ef27916f9828ad52c6
AfroStalin <chernecoff@gmail.com>
2020-07-08 15:51:40 +0300

    [CryDesigner] Added option "Disable physics proxy" for designer objects. Renamed "Exclude collision" to "Exclude collision with player"

--- Code/Sandbox/Plugins/CryDesigner/Objects/DesignerObject.h
+++ Code/Sandbox/Plugins/CryDesigner/Objects/DesignerObject.h
@@ -24,6 +24,7 @@ struct DesignerObjectFlags
 	bool            noDynWater;
 	bool            noStaticDecals;
 	bool            excludeCollision;
+	bool            disablePhysicsProxy;
 	bool            occluder;
 	bool            ignoreTerrainLayerBlend;
 	bool            ignoreDecalBlend;
